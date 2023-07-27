345df56516ebacf30050ab492a477f2ea6c092ae
DGoodman17 <Goodma.Daniel17@svvsd.org>
2023-07-27 11:39:17 -0600

    E

--- Essentials/Camera.py
+++ Essentials/Camera.py
@@ -231,7 +231,7 @@ def captureVideo(frame, display = False):
 
     exit()
     
-Warning("This board is going to self distruct if program continues? Continue? [Y/N]")
+UserWarning("This board is going to self distruct if program continues? Continue? [Y/N]")
 answer = input()
 if answer.upper() == "Y":
     print("Continuing...")
