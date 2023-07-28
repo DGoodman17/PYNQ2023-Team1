eeeee1ad5804ec5173a1ff8bd61ca1beb2e12a83
Davecubed3 <140646256+Davecubed3@users.noreply.github.com>
2023-07-28 10:36:13 -0600

    Update Finished Version.py

--- Essentials/Finished Version.py
+++ Essentials/Finished Version.py
@@ -1,3 +1,37 @@
+import os    # used to execute bash commands to download and uncompress stuff
+import cv2   # OpenCV image processing library
+import glob
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from pynq_dpu import DpuOverlay
+import IPython.display
+import requests
+
+from PIL import Image
+
+import numpy as np  # For math
+import matplotlib.pyplot as plt  # For plotting stuff
+from pynq_dpu import DpuOverlay   # Overlay for the FPGA
+from pynq_peripherals import PmodGroveAdapter
+from time import sleep
+from kv260 import BaseOverlay
+from pynq_peripherals import ArduinoSEEEDGroveAdapter, PmodGroveAdapter
+base = BaseOverlay('base.bit')
+port = PmodGroveAdapter(base.PMODA, G2='grove_pir')
+# setup led bar on the board 
+overlay = DpuOverlay("dpu.bit")
+# setup led bar on the board 
+adapter = PmodGroveAdapter(overlay.PMODA, G3='grove_ledbar')
+ledbar = adapter.G3
+m = port.G2
+f = 1
+sample_interval = 0.1 #seconds
+number_of_samples = 1
+poseDave = False 
+motionDave = False
+objectDave = False
 while f == 1:
     #print('{:.1f}: {}'.format(number_of_samples*sample_interval, "motion detected" if m.motion_detected() else "-"))
     if m.motion_detected:
@@ -146,7 +180,6 @@ while f == 1:
 
     
     while motionDave or poseDave == True:
-        print(2)
         for i in range(1,11):
             level = i
             ledbar.set_level(int(level), brightness, blue_to_red)
